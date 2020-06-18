7c276b159ca0f6504363fffdfd66330faea0cd52
PlasmaDiffusion <Scott50000@gmail.com>
2020-06-18 19:42:30 -0400

    Read in and display words/hints from mongoDB

--- routes/dummy.js
+++ routes/dummy.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {MongoClient} = require('mongodb');
 
 
-var docs = [];
+var docs = {};
 
 //Hello world
 router.get('/', async(req, res, next) => {
@@ -16,7 +16,7 @@ router.get('/', async(req, res, next) => {
     const client = new MongoClient(uri);
 
 
-    var data = {};
+    var crosswordData;
     
     try {
         // Connect to the MongoDB cluster
@@ -28,20 +28,16 @@ router.get('/', async(req, res, next) => {
         // Make the appropriate DB calls
         await  listDatabases(client);
 
-        crosswordData = col.find({});
-        docs = [];
-        await crosswordData.forEach(listWords,errorFunc);
-
-
+        crosswordData = col.find({category:"all"});
+        crosswordData.forEach(listWords,errorFunc);
  
     } catch (e) {
         console.error(e);
     } finally {
         await client.close();
 
-        data.docs = docs;
-        console.log(data);
-        res.render('index', data);
+
+        res.render('index', crosswordData);
     }
 
 
@@ -95,6 +91,84 @@ router.get('/insert', async(req, res, next) => {
    }
 })
 
+router.get('/update', async(req, res, next) => {
+
+    const url = "mongodb+srv://admin:OyDiI2qNWvvVGreF@cluster0-qjfez.mongodb.net/dbname?retryWrites=true&w=majority";
+    const client = new MongoClient(url);
+ 
+    // The database to use
+    const dbName = "test";
+   
+
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "people"
+        const col = db.collection("people");
+
+        // Construct a document                                                                                                                                                              
+        let updatedDocument = {
+            "name": { "first": "Alan", "last": "Turing" },
+            "birth": new Date(2012, 5, 23), // June 23, 1912                                                                                                                                 
+            "death": new Date(2054, 5, 7),  // June 7, 1954                                                                                                                                  
+            "contribs": [ "Turing machine", "Turing test", "Turingery" ],
+            "views": 1250000
+        }
+
+        col.update(
+            {"name.first":"Alan" },   // Query parameter
+            updatedDocument,
+            { upsert: true }      // Options
+         )
+         
+
+       } catch (err) {
+        console.log(err.stack);
+    }
+
+    finally {
+       await client.close();
+
+   }
+})
+
+router.get('/remove', async(req, res, next) => {
+
+    const url = "mongodb+srv://admin:OyDiI2qNWvvVGreF@cluster0-qjfez.mongodb.net/dbname?retryWrites=true&w=majority";
+    const client = new MongoClient(url);
+ 
+    // The database to use
+    const dbName = "test";
+   
+
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+
+        // Use the collection "people"
+        const col = db.collection("people");
+
+
+        col.remove(
+            {"name.first":"Alan" },   // Query parameter
+            { justOne: true }      // Options
+         )
+         
+
+       } catch (err) {
+        console.log(err.stack);
+    }
+
+    finally {
+       await client.close();
+
+   }
+})
+
+
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
  
@@ -104,11 +178,8 @@ async function listDatabases(client){
 
 async function listWords(doc)
 {
-    //console.log(doc);
-    var newDoc = JSON.stringify(doc, null, 4);
-    //docs[(Object.keys(docs).length)] = doc;
-    docs.push(doc);
-    console.log("Doc get!");
+    console.log(JSON.stringify(doc, null, 4));
+    docs.push(JSON.stringify(doc, null, 4));
 };
 
 async function errorFunc(error) {
